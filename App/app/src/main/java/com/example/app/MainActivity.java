09d9a5e646a8f49a798cd36e9dcbc1ef5145c068
Bernardo <benckebernardo@gmail.com>
2023-02-24 11:28:06 -0300

    Atividade 6

---
+++ App/app/src/main/java/com/example/app/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textoNovo;
+    private Integer contador = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textoNovo = (TextView) findViewById(R.id.banana);
+    }
+    public void mudatexto(View v){
+        contador++;
+        textoNovo.setText(Integer.toString(contador));
+    }
+}
